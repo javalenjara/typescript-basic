9b1e96510fa2cca7961a22c2af252b8d406eba6f
Jorge <Jorge.A.Valencia.-ND@disney.com>
2024-02-27 18:34:48 -0500

    Add arrays

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,2 @@
-// We can remove the type annotation as compiler infers it from the assignation type.
-// let sales = 123_456_789;
-let sales: number = 123_456_789;
-let course: string = 'TypeScript';
-let is_published: boolean = true;
-// If we initialize a variable but don't assigna a value, TS assumes its type is 'any'
-let level;
-
-function render(document: any) {
-    console.log(document);
-}
+let numbers: number[] = [1, 2, 3];
+numbers.forEach(n => n.toExponential);
