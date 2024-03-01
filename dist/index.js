69ca7f32252c7324606becf9d07e58cd3a7c8c5c
Jorge <Jorge.A.Valencia.-ND@disney.com>
2024-03-01 18:56:52 -0500

    Optional type access modifier

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,7 @@
 "use strict";
-function kgToLbs(weight) {
-    if (typeof weight === "number") {
-        return weight * 2.2;
-    }
-    else {
-        return parseInt(weight) * 2.2;
-    }
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
 }
+let customer = getCustomer(1);
+console.log(customer === null || customer === void 0 ? void 0 : customer.birthday);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
-function greet(name: string | null | undefined) {
-  if (name) console.log(name.toUpperCase());
-  else console.log("Hola!");
+type Customer = {
+  birthday?: Date;
+};
+
+function getCustomer(id: number): Customer | null {
+  return id === 0 ? null : { birthday: new Date() };
 }
 
-greet(null);
+let customer = getCustomer(1);
+// Optional property access operator
+console.log(customer?.birthday?.getFullYear());
+
+// Optional element access operator
+// customers[0] might be null
+// So customers?.[0]
+
+let log: any = null;
+log?.("a");
