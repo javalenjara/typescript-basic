295ebfb50ebf5a79cf6aa876b0184fd9e034864d
Jorge <Jorge.A.Valencia.-ND@disney.com>
2024-03-01 18:14:59 -0500

    Type alias

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
-let employee: {
-    readonly id: number,
-    name: string,
-    retire: (date: Date) => void
-} = { id: 1, name: 'Jorge', retire: (date: Date) => {
+type Employee = {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+};
+
+let employee: Employee = {
+  id: 1,
+  name: "Jorge",
+  retire: (date: Date) => {
     console.log(date);
-}};
+  },
+};
