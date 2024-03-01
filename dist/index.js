70d655b942901588c5a4d1913c8a942445458150
Jorge <Jorge.A.Valencia.-ND@disney.com>
2024-03-01 18:20:12 -0500

    Types part 2
    * Union types.
    * Narrowing.

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,10 @@
 "use strict";
-var Size;
-(function (Size) {
-    Size[Size["Small"] = 1] = "Small";
-    Size[Size["Medium"] = 2] = "Medium";
-    Size[Size["Large"] = 3] = "Large";
-})(Size || (Size = {}));
-;
-let mySize = Size.Medium;
-console.log(mySize);
+function kgToLbs(weight) {
+    if (typeof weight === "number") {
+        return weight * 2.2;
+    }
+    else {
+        return parseInt(weight) * 2.2;
+    }
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
-type Employee = {
-  readonly id: number;
-  name: string;
-  retire: (date: Date) => void;
-};
-
-let employee: Employee = {
-  id: 1,
-  name: "Jorge",
-  retire: (date: Date) => {
-    console.log(date);
-  },
-};
+// Union types
+function kgToLbs(weight: number | string): number {
+  //Narrowing
+  if (typeof weight === "number") {
+    return weight * 2.2;
+  } else {
+    return parseInt(weight) * 2.2;
+  }
+}
